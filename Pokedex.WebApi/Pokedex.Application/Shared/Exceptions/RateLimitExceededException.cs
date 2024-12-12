066ca0fdb0cd12b176648a3442874a9f46c79a8a
Emanuele Buldrini <emanuele.buldrini@gmail.com>
2024-12-12 22:15:15 +0100

    Remove unsused exception.

--- Pokedex.WebApi/Pokedex.Application/Shared/Exceptions/RateLimitExceededException.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Pokedex.Application.Shared.Exceptions;
-
-public class RateLimitExceededException(string apiName, string? rateLimitDetails = null)
-    : Exception($"Unable to process the request because the {apiName} API rate limit was reached. {rateLimitDetails}");
